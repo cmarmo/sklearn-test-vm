480030240d2ea3a5a6161a2182a864ac3320d98c
Chiara Marmo <chiara.marmo@u-psud.fr>
2020-11-04 16:10:43 +0100

    Update 32bit systems.

--- linux-python36-32/build.sh
+++ linux-python36-32/build.sh
@@ -7,12 +7,13 @@
 set -e
 
 # Install prerequisites.
-apt-get update -y
-apt-get install -y unzip python3-dev libopenmpi-dev python3.4-venv
+#dnf search openmp
+yum install -y unzip python3-devel openmpi
+yum update -y
 
 # Create virtual environment.
 python3 -m venv linux32-python36
-. linux32-python36/bin/activate
+source linux32-python36/bin/activate
 
 # install the wheel
 pip install -U pip
@@ -20,8 +21,6 @@ cd home/
 pip install scikit_learn-0.24.dev0-cp36-cp36m-manylinux2010_i686.whl
 python -c 'import sklearn; sklearn.show_versions()'
 
-# Clean up.
-apt-get autoremove -y
-apt-get clean
-rm -rf /var/lib/apt/lists/partial/* /tmp/* /var/tmp/*
+yum autoremove -y
+yum clean all
 
